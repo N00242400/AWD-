73f702d15ecb8803ce026289d5a03cf7a69e4a1f
Caitlin Horan <N00242400@iadt.ie>
2025-09-30 16:44:20 +0100

    fixing iimage error

--- my-pet-project/resources/views/components/pet-details.blade.php
+++ my-pet-project/resources/views/components/pet-details.blade.php
@@ -2,11 +2,12 @@
 
 <div class="max-w-sm bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300">
     <img 
-        src="{{ asset('images/', $image) }}" 
-        alt="{{ $name }}" 
+        src="{{ asset('images/' . $image) }}" 
+        alt="{{ $image }}" 
         class="w-full h-48 object-cover"
-        {{-- onerror="this.onerror=null;this.src='https://via.placeholder.com/400x300?text=No+Image';" --}}
+     
     >
+
     <div class="p-4">
         <h3 class="text-xl font-semibold text-gray-800 mb-1">{{ $name }}</h3>
 
