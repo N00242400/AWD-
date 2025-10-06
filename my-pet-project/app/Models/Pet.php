48c9921d7419d2b596ec3e292856eaac9c1a928f
Caitlin Horan <N00242400@iadt.ie>
2025-10-06 14:44:54 +0100

    editing dahsboard

--- my-pet-project/app/Models/Pet.php
+++ my-pet-project/app/Models/Pet.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+//creating a pet model//
 class Pet extends Model
 {
+   //trait you add to models (allows you to generate fake users) //
    use HasFactory;
-
+//mass assignment protection feature//
+//Attributes not in this list will be ignored//
     protected $fillable = [
         'name',
         'age',
--- my-pet-project/resources/views/dashboard.blade.php
+++ my-pet-project/resources/views/dashboard.blade.php
@@ -5,9 +5,10 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+<div class="py-12" style="background-image: url('/images/bg.jpg'); background-size: contain; background-repeat: no-repeat; background-position: center; min-height: 100vh;">
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white bg-opacity-80 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
