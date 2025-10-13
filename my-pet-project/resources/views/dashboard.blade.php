783964a2caa3232ee0435084cd75d4cceb918d3e
Caitlin Horan <N00242400@iadt.ie>
2025-10-13 14:46:22 +0100

    styling my dashboard

--- my-pet-project/resources/views/dashboard.blade.php
+++ my-pet-project/resources/views/dashboard.blade.php
@@ -1,18 +1,18 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-<div class="py-12" style="background-image: url('/images/bg.jpg'); background-size: cover; background-repeat: no-repeat; background-position: center; min-height: 100vh;">
-
+<div class="py-12 bg-white" >
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white bg-opacity-80 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+             <div class="p-6 text-gray-900">
+                   <h1 class="text-4xl font-extrabold dark:text-white">WELCOME TO THE PET DATABASE</h1>
                 </div>
-            </div>
+            <div class="bg" style="background-image: url('/images/dashboard.jpg');  background-repeat: no-repeat; background-position: center; min-height: 100vh;">
+                </div>
+
+              <!--  <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }} 
+                </div> -->
+
         </div>
     </div>
 </x-app-layout>
+
--- my-pet-project/resources/views/pets/index.blade.php
+++ my-pet-project/resources/views/pets/index.blade.php
@@ -1,9 +1,8 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="py-12" style="background-image: url('/images/bg.jpg'); background-size: cover; background-repeat: no-repeat; background-position: center; min-height: 100vh;">>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h3 class="font-semibold text-lg mb-4">List Of Pets:</h3>
                     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                         @foreach($pets as $pet)
                         <div class="border-4 rounded-lg shadow-md">
