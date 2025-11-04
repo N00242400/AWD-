3817ba95d689405fc4096a26777decb4c818ff03
n00242400 <n00242400@iadt.ie>
2025-11-04 13:32:29 +0000

    Seedinguser table with an admin user

--- my-pet-project/database/migrations/2025_11_04_132629_add_role_to_users_table.php
+++ my-pet-project/database/migrations/2025_11_04_132629_add_role_to_users_table.php
@@ -9,20 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(){
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('role')->default('user'); //default to user for regular users//
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(){
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('role');
         });
     }
 };
