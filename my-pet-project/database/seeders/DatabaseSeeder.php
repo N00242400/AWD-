49c573afc66cfee43e0df2d521c113c35a4c24b5
caitlin <n00242400@iadt.ie>
2025-11-12 19:29:35 +0000

    Update DatabaseSeeder to include OwnerSeeder so owners are seeded along with pets

--- my-pet-project/database/seeders/DatabaseSeeder.php
+++ my-pet-project/database/seeders/DatabaseSeeder.php
@@ -8,13 +8,12 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+   //method that takes one or more seeder classes and runs them in order.//
     public function run(): void
     {
-       
-       
+       //Runs the PetSeeder to add sample pets to the 'pets' table//
         $this->call(PetSeeder::class);
+        //Runs the OwnerSeeder to add sample owners to the 'owners' table
+        $this->call(OwnerSeeder::class);
     }
 }
